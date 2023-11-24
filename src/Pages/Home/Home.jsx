4914aaf7b549c8cc203e49432a2d4ec22464a006
mahabubr <mahabub.r753@gmail.com>
2023-11-24 17:18:59 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="bg-light">Home</div>;
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-undef
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#03001C",
+      secondary: "#301E67",
+      accent: "#5B8FB9",
+      light: "#B6EADA",
+      white: "#FFFBF5",
+    },
+  },
+  plugins: [],
+});
