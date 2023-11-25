2f75d827a6e1c12495be596ee0fccc679c5eee03
mahabubr <mahabub.r753@gmail.com>
2023-11-25 18:43:19 +0600

    Event Schedule DONE

--- src/Component/Home/EventSchedule.jsx
+++ src/Component/Home/EventSchedule.jsx
@@ -1,18 +1,49 @@
+import Loader from "../Shared/Loader/Loader";
 import Title from "../Shared/Title/Title";
+import { useQuery, gql } from "@apollo/client";
+import EventCard from "../UI/Card/EventCard";
+
+const GET_EVENTS = gql`
+  query Events {
+    Events {
+      _id
+      category
+      description
+      hall_no
+      image
+      location
+      name
+      price
+      seat_no
+      time
+      time_from
+    }
+  }
+`;
 
 const EventSchedule = () => {
+  const { loading, error, data } = useQuery(GET_EVENTS);
+
+  const events = data?.Events.slice(0, 3);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div className="text-white mt-20 container w-11/12 mx-auto">
+    <div className=" mt-20 container w-11/12 mx-auto">
       <Title content="Event Schedule" />
-      <p className="text-sm mt-2 text-center text-accent">
+      <p className="text-sm mt-2 text-justify text-accent md:w-8/12 mx-auto">
         Join us for an exciting lineup of speakers and workshops in our event
         schedule, promising insightful discussions and engaging activities for
         all attendees. Explore a diverse range of sessions and networking
         opportunities in our meticulously crafted event schedule, designed to
         inspire and empower participants.
       </p>
-      <div>
-        
+      <div className="mt-10 backdrop-blur-lg bg-white/20 rounded-lg border border-primary border-dashed">
+        {!error &&
+          events.length > 0 &&
+          events.map((event, i) => <EventCard key={i} event={event} />)}
       </div>
     </div>
   );
---
+++ src/Component/UI/Card/EventCard.jsx
@@ -0,0 +1,44 @@
+import { Button } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+import { FaCalendar } from "react-icons/fa";
+import { LiaSearchLocationSolid } from "react-icons/lia";
+
+const EventCard = ({ event }) => {
+  return (
+    <div className="grid lg:grid-cols-5 gap-20 p-6  border-b pb-8 border-primary border-dashed">
+      <div className="lg:col-span-1  lg:border-r lg:pe-4 border-primary border-dashed">
+        <img
+          src={event.image}
+          alt=""
+          className="rounded-md w-full h-full object-cover lg:border p-1 border-primary border-dashed bg-white"
+        />
+      </div>
+      <div className="lg:col-span-3  ">
+        <h2 className="text-xl font-bold text-white">{event.name}</h2>
+        <p className="mt-2 text-gray-300">{event.description}</p>
+        <div className="mt-8 flex justify-between text-sm font-bold flex-wrap text-light-green-200">
+          <p className="text-center flex items-center gap-2">
+            <FaCalendar />
+            {new Date(event.time).toDateString()}
+          </p>
+          <p className="text-center flex items-center gap-2">
+            <LiaSearchLocationSolid />
+            {event.location} - {event.hall_no} - {event.seat_no}
+          </p>
+          <p className="text-center flex items-center gap-2">$ {event.price}</p>
+        </div>
+      </div>
+      <div className="lg:col-span-1 lg:border-l  border-primary border-dashed">
+        <div className="flex justify-center items-center h-full w-full">
+          <Link to={`/event/${event._id}`} >
+            <Button variant="gradient" color="deep-orange">
+              Details
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
